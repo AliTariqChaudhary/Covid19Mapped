021e48937395d437e8c34565293f822c4008384a
Ali <chaudharyali392@gmail.com>
2020-04-29 06:14:21 -0700

    formatted data correctly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const covid19api = require('covid19-api');
 const ejs = require('ejs');
 const https = require('https');
 var fs = require('fs');
-
+const d3 = require('d3');
 
 //firt date is 2020-01-22T00:00:00Z
 var firstD = new Date('January 22, 2020 00:00:00 GMT+00:00');
@@ -12,18 +12,26 @@ var today = new Date(new Date().toUTCString());
 diff = Math.floor((today.getTime()- firstD.getTime())/(1000*3600*24));
 todayString = today.toDateString();
 
-var countryDict = {'AF':{}, 'AL':{},'DZ':{}/*,'AD':{},'AO':{},'AG':{},'AR':{},'AM':{},'AU':{},'AT':{},'AZ':{},'BS':{},'BH':{},'BD':{},'BB':{},'BY':{},'BE':{},
-'BZ':{},'BJ':{},'BT':{},'BO':{},'BA':{},'BW':{},'BR':{},'BN':{},'BG':{},'BF':{},'BI':{},'CV':{},'KH':{},'CM':{},'CA':{},'CF':{},'TD':{},'CL':{},'CN':{},'CO':{},'KM':{},'CG':{},
-'CD':{},'CR':{},'CI':{},'HR':{},'CU':{},'CY':{},'CZ':{},'DK':{},'DJ':{},'DM':{},'DO':{},'EC':{},'EG':{},'SV':{},'EQ':{},'ER':{},'EE':{},'SZ':{},'ET':{},'FJ':{},'FI':{},'FR':{},
-'GA':{},'GM':{},'GE':{},'DE':{},'GH':{},'GR':{},'GD':{},'GT':{},'GN':{},'GW':{},'GY':{},'HT':{},'VA':{},'HN':{},'HU':{},'IS':{},'IN':{},'ID':{},'IR':{},'IQ':{},'IE':{},'IL':{},
-'IT':{},'JM':{},'JP':{},'JM':{},'JP':{},'JP':{},'JO':{},'KZ':{},'KE':{},'KI':{},'KP':{},'KR':{},'KW':{},'KG':{},'LA':{},'LV':{},'LB':{},'LS':{},'LR':{},'LY':{},'LI':{},'LT':{},
-'LU':{},'MG':{},'MW':{},'MY':{},'MV':{},'ML':{},'MT':{},'MH':{},'MR':{},'MU':{},'MX':{},'FM':{},'MD':{},'MC':{},'MN':{},'ME':{},'MA':{},'MZ':{},'MM':{},'NA':{},'NR':{},'NP':{},
-'NL':{},'NZ':{},'NI':{},'NE':{},'NG':{},'MK':{},'NO':{},'OM':{},'PK':{},'PW':{},'PA':{},'PG':{},'PY':{},'PE':{},'PH':{},'PL':{},'PT':{},'QA':{},'RO':{},'RU':{},'RW':{},'KN':{},
-'LC':{},'VC':{},'WS':{},'SM':{},'ST':{},'SA':{},'SN':{},'RS':{},'SC':{},'SL':{},'SG':{},'SK':{},'SI':{},'SB':{},'SO':{},'ZA':{},'SS':{},'ES':{},'LK':{},'SD':{},'SR':{},'SE':{},
-'CH':{},'SY':{},'TJ':{},'TZ':{},'TH':{},'TL':{},'TG':{},'TO':{},'TT':{},'TN':{},'TR':{},'TM':{},'TV':{},'UG':{},'UA':{},'AE':{},'GB':{},'US':{},'UY':{},'UZ':{},'VU':{},'VE':{},
-'VN':{},'YE':{},'ZM':{}*/}
+var countryDict = {'Afghanistan':{}, 'Albania':{},'Algeria':{},'Andorra':{},'Angola':{},'Antigua and Barbuda':{},'Argentina':{},'Armenia':{},'Australia':{},'Austria':{},'Azerbaijan':{},'Bahamas':{},'Bahrain':{},'Bangladesh':{},'Barbados':{},'Belarus':{},'Belgium':{},
+'Belize':{},'Benin':{},'Bhutan':{},'Bolivia':{},'Bosnia':{},'Botswana':{},'Brazil':{},'Brunei Darussalam':{},'Bulgaria':{},'Burkina Faso':{},'Burundi':{},'Cabo Verde':{},'Cambodia':{},'Cameroon':{},'Canada':{},'Central African Republic':{},'Chad':{},'Chile':{},'China':{},'Colombia':{},'Comoros':{},'Republic of the Congo':{},
+'Democratic Republic of the Congo':{},'Costa Rica':{},"CI":{},'Croatia':{},'Cuba':{},'Cyprus':{},'Czechia':{},'Denmark':{},'Djibouti':{},'Dominica':{},'Dominican Republic':{},'Ecuador':{},'Egypt':{},'El Salvador':{},'Equatorial Guinea':{},'Eritrea':{},'Estonia':{},'Eswatini':{},'Ethiopia':{},'Fiji':{},'Finland':{},'France':{},
+'Gabon':{},'Gambia':{},'Georgia':{},'Germany':{},'Ghana':{},'Greece':{},'Grenada':{},'Guatemala':{},'Guinea':{},'Guinea-Bissau':{},'Guyana':{},'Haiti':{},'Holy See':{},'Honduras':{},'Hungary':{},'Iceland':{},'India':{},'Indonesia':{},'Iran':{},'Iraq':{},'Ireland':{},'Israel':{},
+'Italy':{},'Jamaica':{},'Japan':{},'Jordan':{},'Kazakhstan':{},'Kenya':{},'Kiribati':{},'South Korea':{},'Kuwait':{},'Kyrgyzstan':{},"Laos":{},'Latvia':{},'Lebanon':{},'Lesotho':{},'Liberia':{},'Libya':{},'Liechtenstein':{},'Lithuania':{},'Luxembourg':{},'Madagascar':{},'Malawi':{},
+'Malaysia':{},'Maldives':{},'Mali':{},'Marshall Islands':{},'Malta':{},'Mauritania':{},'Mauritius':{},'Mexico':{},'Micronesia':{},'Moldova':{},'Monaco':{},'Mongolia':{},'Montenegro':{},'Morocco':{},'Mozambique':{},'Myanmar':{},'Namibia':{},'Nauru':{},'Nepal':{},
+'Netherlands':{},'New Zealand':{},'Nicaragua':{},'Niger':{},'Nigeria':{},'North Macedonia':{},'Norway':{},'Oman':{},'Pakistan':{},'Palau':{},'Panama':{},'Papua New Guinea':{},'Paraguay':{},'Peru':{},'Philippines':{},'Poland':{},'Portugal':{},'Qatar':{},'Romania':{},'Russian Federation':{},'Rwanda':{},'Saint Kitts and Nevis':{},
+'Saint Lucia':{},'Saint Vincent and the Grenadines':{},'Samoa':{},'San Marino':{},'Sao Tome and Principe':{},'Saudi Arabia':{},'Senegal':{},'Serbia':{},'Seychelles':{},'Sierra Leone':{},'Singapore':{},'Slovakia':{},'Slovenia':{},'Solomon Islands':{},'Somalia':{},'South Africa':{},'South Sudan':{},'Spain':{},'Sri Lanka':{},'Sudan':{},'Suriname':{},'Sweden':{},
+'Switzerland':{},'Syria':{},'Tajikistan':{},'Tanzania, United Republic of':{},'Thailand':{},'Timor-Leste':{},'Togo':{},'Tonga':{},'Trinidad and Tobago':{},'Tunisia':{},'Turkey':{},'Turkmenistan':{},'Tuvalu':{},'Uganda':{},'Ukraine':{},'United Arab Emirates':{},'United Kingdom':{},'United States of America':{},'Uruguay':{},'Uzbekistan':{},'Vanuatu':{},'Venezuela':{},
+'Viet Nam':{},'Yemen':{},'Zambia':{}, 'Zimbabwe': {}}
 var parsedData;
-
+console.log(new Date(new Date().toLocaleString("en-US", {timezone: "UTC"})));
+var DictFromSave;
+fs.readFile("countryDict.txt", (err, data)=>{
+    if(err) console.log(err);
+    else{ 
+        DictFromSave = JSON.parse(data);
+        console.log('File read.');
+    }
+ });
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -132,22 +140,24 @@ function doRequest(key) {
 async function getData(){
 
     for(var key in countryDict){
+        console.log(key);
         await doRequest(key);
         await doRequest2(key);
     }
+    fs.writeFile("countryDict.txt", JSON.stringify(countryDict), 'binary', (err)=>{
+        if(err) console.log(err)
+        else console.log('File saved')
+     });
 } 
 
 //getData();
 
 
 
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    /*fs.writeFile("countryDict.txt", JSON.stringify(countryDict), 'binary', (err)=>{
-        if(err) console.log(err)
-        else console.log('File saved')
-     })*/
     res.render('index', {totalDays: diff+1, todayString: todayString});
 });
 
